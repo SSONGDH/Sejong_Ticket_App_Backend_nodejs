301e8cb89a60a371a1a9381c88b81496a5a9802d
SSong_dae_hyeon <sky900156@naver.com>
2025-04-09 23:30:47 +0900

    FEAT: 티켓메인 화면에서 카드 클릭시 환불 상세내역 반환 코드 추가

--- src/service/index.js
+++ src/service/index.js
@@ -24,6 +24,7 @@ import paymentPermissionRoutes from "../routes/payment/paymentPermission.js";
 import adminConnectionRoutes from "../routes/admin/adminConnection.js";
 import refundDenyRoutes from "../routes/refund/refundDeny.js";
 import paymentDenyRoutes from "../routes/payment/paymentDeny.js";
+import ticketRefundDetailRoutes from "../routes/ticket/ticketRefundDetail.js";
 import fcmTokenRoutes from "../routes/FCM/fcmTokenAdd.js"; // ✅ FCM 토큰 저장 API 추가
 import cronJob from "../jobs/cronJob.js"; // ✅ 크론 작업 실행
 import cookieParser from "cookie-parser"; // 쿠키 파서 미들웨어
@@ -76,6 +77,7 @@ app.use(adminConnectionRoutes);
 app.use(ticketAddNFCRoutes);
 app.use(refundDenyRoutes);
 app.use(paymentDenyRoutes);
+app.use(ticketRefundDetailRoutes);
 // app.use(fcmTokenRoutes);
 
 //cronJob(); // ✅ 크론 작업 실행
