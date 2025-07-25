cd0aa378da32094ef0db734a468c3563c28cf4a8
송대현 <sky900156@naver.com>
2025-07-25 15:18:02 +0900

    TEST: 현재경로 출력

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
+console.log("Current working directory:", process.cwd());
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 import mongoose from "mongoose";
