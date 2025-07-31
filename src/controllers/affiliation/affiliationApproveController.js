98fb319e70f96eff6391a12d3ecc105da77b7a4a
송대현 <sky900156@naver.com>
2025-07-31 22:29:53 +0900

    DEBUG: 소속 및 권한 함수 경로 설정

--- src/controllers/affiliation/affiliationApproveController.js
+++ src/controllers/affiliation/affiliationApproveController.js
@@ -1,16 +1,22 @@
+import { handleAffiliationApproval } from "../../services/affiliationApproveService.js";
+
 export const approveAffiliationRequest = async (req, res) => {
   try {
-    const requestId = req.query.requestId; // 쿼리 파라미터로 받음
-
+    const requestId = req.query.requestId;
     if (!requestId) {
       return res.status(400).json({ message: "requestId가 필요합니다." });
     }
 
     const result = await handleAffiliationApproval(requestId);
-    res.status(200).json(result);
+    res.status(200).json({
+      code: "SUCCESS-0000",
+      message: "소속 승인 완료",
+      result,
+    });
   } catch (err) {
-    res
-      .status(500)
-      .json({ message: "소속 승인 중 오류 발생", error: err.message });
+    res.status(500).json({
+      message: "소속 승인 중 오류 발생",
+      error: err.message,
+    });
   }
 };
