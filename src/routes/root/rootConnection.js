1b8eb49faafbd33c98d0976dfd4469426b0e33f4
송대현 <sky900156@naver.com>
2025-08-01 00:18:48 +0900

    FEAT: 개발자 모드 권한 확인 관련 디버그

---
+++ src/routes/root/rootConnection.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { rootConnection } from "../../controllers/root/rootConnectionController.js";
+
+const router = express.Router();
+
+router.get("/connection", rootConnection);
+
+// ✅ default export 추가
+export default router;
