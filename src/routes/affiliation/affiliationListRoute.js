04c77b9e926696a72773dabbb83e9b774faf0aae
송대현 <sky900156@naver.com>
2025-08-09 02:33:23 +0900

    DEBUG: 소속 반환 경로 설정2

--- src/routes/affiliation/affiliationListRoute.js
+++ src/routes/affiliation/affiliationListRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { approveAffiliationList } from "../../controllers/affiliation/affiliationListController.js";
+import { getAffiliationListController } from "../../controllers/affiliation/affiliationListController.js";
 
 const router = express.Router();
 
 // 쿼리 파라미터로 받도록 경로 수정
-router.post("/List", approveAffiliationList);
+router.get("/List", getAffiliationListController);
 
 export default router;
