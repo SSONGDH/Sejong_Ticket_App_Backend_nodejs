3469aff2f8745ca0b8a495827916012b7c97d79f
송대현 <sky900156@naver.com>
2025-08-25 15:05:37 +0900

    FEAT: 신청 상세화면

--- src/routes/affiliation/affiliationRequestsListRoute.js
+++ src/routes/affiliation/affiliationRequestsListRoute.js
@@ -1,10 +1,18 @@
-// route/affiliation/affiliationRequestsListRoute.js
+// src/routes/affiliation/affiliationRequestsListRoute.js
 import express from "express";
-import { affiliationRequestsListController } from "../../controllers/affiliation/affiliationRequestListController.js";
+import {
+  affiliationRequestListController,
+  affiliationRequestDetailController,
+} from "../../controllers/affiliation/affiliationRequestListController.js";
 
 const router = express.Router();
 
-// 소속 신청 리스트 조회
-router.get("/affiliationRequestsList", affiliationRequestsListController);
+// 전체 소속 신청 목록 조회
+// GET /affiliation/affiliationRequestsList?status=pending
+router.get("/affiliationRequestsList", affiliationRequestListController);
+
+// 특정 소속 신청 상세 조회
+// GET /affiliation/affiliationRequests/:id
+router.get("/affiliationRequests/:id", affiliationRequestDetailController);
 
 export default router;
