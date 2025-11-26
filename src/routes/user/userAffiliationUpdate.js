688b4f6bbfe6e874c8ee09f3badf2eee6be8fb3a
송대현 <sky900156@naver.com>
2025-11-26 14:55:58 +0900

    DEBUG: 소속 업데이트 관련 디버깅

--- src/routes/user/userAffiliationUpdate.js
+++ src/routes/user/userAffiliationUpdate.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { updateAffiliation } from "../../controllers/user/mypageController.js";
+// [1] 미들웨어 불러오기
+import { authenticate } from "../../middlewares/authMiddleware.js"; 
 
 const router = express.Router();
 
-router.put("/affiliationUpdate", updateAffiliation);
+// [2] 미들웨어(authenticate) 장착!
+// 이제 문지기가 먼저 유저 정보를 확인하고 req.user를 만들어줍니다.
+router.put("/affiliationUpdate", authenticate, updateAffiliation);
 
 export default router;
